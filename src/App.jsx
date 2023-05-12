fbe43358eb97a552760622731562caabc5c4a1cd
Dario Mercado <dmercado@mavenlink.com>
2023-05-12 13:51:27 -0700

    feat: add input background color

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,52 @@ function App() {
             <Grid>
                 <Grid.Col span={4}>
                     <Flex align="center" justify="center" mih="100%">
-                        <TextInput label="Character Name" sx={{ border: '1px solid black' }} w="100%" p="8px"/>
+                        <TextInput
+                            label="Character Name"
+                            sx={{ border: '1px solid black' }}
+                            w="100%"
+                            p="8px"
+                            variant="filled"
+                        />
                     </Flex>
                 </Grid.Col>
                 <Grid.Col span={8} sx={{ border: '1px solid black' }}>
                     <Grid>
                         <Grid.Col span={4}>
-                            <TextInput label="Class & Level"/>
+                            <TextInput
+                                label="Class & Level"
+                                variant="filled"
+                            />
                         </Grid.Col>
                         <Grid.Col span={4}>
-                            <TextInput label="Background"/>
+                            <TextInput
+                                label="Background"
+                                variant="filled"
+                            />
                         </Grid.Col>
                         <Grid.Col span={4}>
-                            <TextInput label="Player Name"/>
+                            <TextInput
+                                label="Player Name"
+                                variant="filled"
+                            />
                         </Grid.Col>
                         <Grid.Col span={4}>
-                            <TextInput label="Race"/>
+                            <TextInput
+                                label="Race"
+                                variant="filled"
+                            />
                         </Grid.Col>
                         <Grid.Col span={4}>
-                            <TextInput label="Alignment"/>
+                            <TextInput
+                                label="Alignment"
+                                variant="filled"
+                            />
                         </Grid.Col>
                         <Grid.Col span={4}>
-                            <TextInput label="Experience Points"/>
+                            <TextInput
+                                label="Experience Points"
+                                variant="filled"
+                            />
                         </Grid.Col>
                     </Grid>
                 </Grid.Col>
